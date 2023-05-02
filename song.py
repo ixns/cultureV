e6c9c7ab8b7cc6c662a36cab5a9eaf93fb6ae80b
3 <74507871+ixns@users.noreply.github.com>
2023-05-02 18:04:19 -0400

    added metadata function and various other touch ups

--- song.py
+++ song.py
@@ -27,10 +27,18 @@ class Song:
             ydl.download('https://www.youtube.com/watch?v=%s' % ytid)
             return output_file
 
+    #TODO: implement this function
     def _pull_lyrics(self):
         return 0
 
-    def _find_metadata_from_youtube(self):
+    #metadata = {
+    #    "title": "Example Song",
+    #    "artist": "Example Artist",
+    #    "album": "Example Album",
+    #    "genre": "Example Genre",
+    #    "year": "2023",
+    #}
+    def _find_metadata(self):
         #This function can be built using example_search_output.txt
         #because that's the data it will work with
         return 0
@@ -52,34 +60,81 @@ class Song:
         with open(output_file, 'wb') as handler:
             handler.write(img_data)
         return output_file
+    
+    def _download_thumbnail_from_lastFM(self, path:str):
+        from bs4 import BeautifulSoup
+        string = f"{self.title} {self.name}"
+        r = requests.get(f"https://last.fm/search?q={string.replace(' ', '+')}")
+        soup = BeautifulSoup(r.text, 'html.parser')
+        cover_art_results = soup.find_all("a", class_="cover-art")
+        try:
+            first_result = cover_art_results[0]
+        except IndexError:
+            return None
+        first_thumbnail = first_result.img
+        return first_thumbnail["src"].replace("u/64s", "u/128s") 
 
-    def _enrich_audio_file_with_thumbnail_and_metadata(self):
-        return 0
+    def _enrich_audio_file_with_thumbnail_and_metadata(self, thumbnail_url=None):
+        from pydub import AudioSegment
+        from mutagen.mp3 import MP3
+        from mutagen.id3 import ID3, TIT2, TPE1, TALB, TCON, TDRC, APIC
+        
+        # Convert audio to MP3
+        audio = AudioSegment.from_file(self.song_file)
+        audio.export("output.mp3", format="mp3")
+
+        # Load the MP3 file and its metadata
+        mp3_file = MP3("output.mp3", ID3=ID3)
+
+        # Add metadata
+        if "title" in self.metadata:
+            mp3_file["TIT2"] = TIT2(encoding=3, text=self.metadata["title"])
+        if "artist" in self.metadata:
+            mp3_file["TPE1"] = TPE1(encoding=3, text=self.metadata["artist"])
+        if "album" in self.metadata:
+            mp3_file["TALB"] = TALB(encoding=3, text=self.metadata["album"])
+        if "genre" in self.metadata:
+            mp3_file["TCON"] = TCON(encoding=3, text=self.metadata["genre"])
+        if "year" in self.metadata:
+            mp3_file["TDRC"] = TDRC(encoding=3, text=self.metadata["year"])
+
+        # Add album cover
+        if thumbnail_url:
+            response = requests.get(thumbnail_url)
+            if response.status_code == 200:
+                album_cover_data = response.content
+
+                mp3_file["APIC"] = APIC(
+                    encoding=3,
+                    mime="image/jpeg",  # Change to 'image/png' if using a PNG file
+                    type=3,  # Type 3 is for album/cover front
+                    desc="Cover",
+                    data=album_cover_data,
+                )
+
+        # Save the MP3 file with metadata
+        mp3_file.save()
+
+        # Read the MP3 file as binary data
+        with open("output.mp3", "rb") as f:
+            mp3_blob = f.read()
+
+        return mp3_blob
 
     def __init__(self, title:str, artist:str):
         self.title = title
         self.artist = artist
-        
         results = search_videos(f"{title} {artist}", 1)
         self.song_information_from_youtube = results[0]
-        
         self.song_file = self._download_audio_from_youtube(f"./{title}_{artist}")
-        self.thumbnail_file = self._download_thumbnail_from_youtube("./")
-        self.metadata = self._find_metadata_from_youtube()
-        # metadata should be like: {
-        #   "title": "Gastronomie",   
-        #   "artist": "Mach Hommy",
-        #   "album": "Dollar Menu 4",
-        #   "year": "2022",
-        #   "genre": "rap",
-        #}
+        self.thumbnail_url = self._download_thumbnail_from_youtube("./")
+        self.metadata = self._find_metadata()
+        self.final_mp3_blob = self._enrich_audio_file_with_thumbnail_and_metadata(self.thumbnail_url)
 
-        self._enrich_audio_file_with_thumbnail_and_metadata()
-
-        try:
-            self.lyrics = self._pull_lyrics()
-        except LyricsNotFound:
-            self.lyrics = "Lyrics unavailable"
+        #try:
+        #    self.lyrics = self._pull_lyrics()
+        #except LyricsNotFound:
+        #    self.lyrics = "Lyrics unavailable"
 
     def outputToHTML(self, path:str):
         env = Environment(
@@ -87,6 +142,9 @@ class Song:
             autoescape=select_autoescape()
         )
 
+        # Save the MP3 blob to a file
+        with open(f"{path}/{self.title}_{self.artist}.mp3", "wb") as f:
+            f.write(self.final_mp3_blob)
         output_file = f"{path}/{self.title}_{self.artist}.html"
 
         template = env.get_template("song_template.html")
